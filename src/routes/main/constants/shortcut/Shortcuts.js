a04ae80e760161e809b0feb2c8fbd241280bbab4
pookey1104 <pookey1104@naver.com>
2025-08-31 05:19:35 +0900

    🔥 Remove: 오시는길 삭제 (#9)

--- src/routes/main/constants/shortcut/Shortcuts.js
+++ src/routes/main/constants/shortcut/Shortcuts.js
@@ -2,7 +2,6 @@ import IcInsta from '@assets/main/ic-insta-40.svg?react';
 import IcYoutube from '@assets/main/ic-youtube-40.svg?react';
 import IcMail from '@assets/main/ic-mail-40.svg?react';
 import IcPhone from '@assets/main/ic-phone-40.svg?react';
-import IcDirection from '@assets/main/ic-direction-40.svg?react';
 
 export const SHORTCUTS = (setIsModalOpen) => [
   {
@@ -21,6 +20,4 @@ export const SHORTCUTS = (setIsModalOpen) => [
   },
   { Icon: IcMail, shortcut: '메일', onClick: () => setIsModalOpen('메일') },
   { Icon: IcPhone, shortcut: '전화', onClick: () => setIsModalOpen('전화') },
-  // TODO: '오시는길' 확인 후 추가
-  { Icon: IcDirection, shortcut: '오시는길', onClick: () => {} },
 ];
