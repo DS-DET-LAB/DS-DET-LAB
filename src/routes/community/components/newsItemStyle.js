5c24eac60bb1e492f6445bec3e9eafa7513cfb95
mzxxzysy <jsyjj21@gmail.com>
2025-09-01 22:38:44 +0900

    💄 Design: newsItem 타이틀 높이 고정 (#10)

--- src/routes/community/components/newsItemStyle.js
+++ src/routes/community/components/newsItemStyle.js
@@ -53,9 +53,19 @@ export const Title = styled.div`
   font-weight: 500;
   letter-spacing: -0.5px;
   align-self: stretch;
+
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  height: 52px;
+
   @media (max-width: 767px) {
     font-size: 14px;
     font-weight: 400;
+    height: 37px;
   }
 `;
 
