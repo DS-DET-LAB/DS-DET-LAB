fb2df36dfeccdbbd8cba9f5bbe369d762e177e53
pookey1104 <pookey1104@naver.com>
2025-09-04 18:26:43 +0900

    ♻️ Refactor: InformModal zIndex 수정 (#9)

--- src/routes/main/components/inform-modal/informModalStyle.js
+++ src/routes/main/components/inform-modal/informModalStyle.js
@@ -6,7 +6,7 @@ export const Background = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  z-index: 10;
+  z-index: 1500;
   background-color: ${palette.background.black25};
   width: 100vw;
   height: 100dvh;
